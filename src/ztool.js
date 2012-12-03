0fa75a9c75d793b065eb3d5468b13824c502640a
maisui99 <maisui99@gmail.com>
2012-12-03 14:58:09 +0800

    Signed-off-by: maisui99 <maisui99@gmail.com>

--- src/ztool.js
+++ src/ztool.js
@@ -1,42 +1,46 @@
+/**
+ * 工具方法
+ */
+
 var fs = require('fs'),
     path = require('path');
 
 var toString = Object.prototype.toString;
 
-exports.is = function(type, obj) {
+exports.is = function (type, obj) {
     var clas = toString.call(obj).slice(8, -1);
     return obj !== undefined && obj !== null && clas === type;
-}
+};
 
-exports.isString = function(obj){
+exports.isString = function (obj) {
     return toString.call(obj) === '[object String]';
-}
+};
 
-exports.isArray = Array.isArray || function(obj){
+exports.isArray = Array.isArray || function (obj) {
     return toString.call(obj) === '[object Array]';
-}
+};
 
-exports.isArguments = function(obj){
+exports.isArguments = function (obj) {
     return toString.call(obj) === '[object Arguments]';
-}
+};
 
-exports.isObject = function(obj){
+exports.isObject = function (obj) {
     return toString.call(obj) === '[object Object]';
-}
+};
 
-exports.isFunction = function(obj){
+exports.isFunction = function (obj) {
     return toString.call(obj) === '[object Function]';
-}
+};
 
-exports.isUndefined = function(obj){
+exports.isUndefined = function (obj) {
     return toString.call(obj) === '[object Undefined]';
-}
+};
 
-exports.endsWith = function(str, end){
+exports.endsWith = function (str, end) {
     var index = str.lastIndexOf(end);
     return index + end.length == str.length;
-}
+};
 
-exports.jsonParse = function(jsonStr){
+exports.jsonParse = function (jsonStr) {
     return Function('return ' + jsonStr)();
-}
+};
