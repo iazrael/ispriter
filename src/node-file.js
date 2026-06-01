ea8f45c65b4402d816a9e43db98ca75fa10d306b
Azrael <azrael@imatlas.com>
2026-06-01 08:47:46 +0800

    fix: node-file.js, BackgroundInterpreter.js - throw Error objects, add use strict

--- src/node-file.js
+++ src/node-file.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fs = require('fs'),
     path = require('path'),
     util = require('util')
@@ -104,7 +106,7 @@ var copyFileSync = function (src, dst, overwrite) {
     var stat, input, output;
     // console.log('coping ' + src);
     if(!fs.existsSync(src)){
-        throw 'File ' + src + ' is not exists.';
+        throw new Error('File ' + src + ' is not exists.');
     }
     //创建目标目录
     mkdirsSync(path.dirname(dst));
@@ -121,7 +123,7 @@ var copyFileSync = function (src, dst, overwrite) {
         }
         if(stat.isFile() && !overwrite){
             //是个文件且不能覆盖
-            throw 'File ' + dst + ' is exists.';
+            throw new Error('File ' + dst + ' is exists.');
         }
     }else{
         if(isDirectoryPath(dst)){
@@ -145,7 +147,7 @@ var copyFile = function (src, dst, overwrite, callback) {
     var stat, input, output;
     // console.log('coping ', src, 'to', dst);
     if(!fs.existsSync(src)){
-        throw 'File ' + src + ' is not exists.';
+        throw new Error('File ' + src + ' is not exists.');
     }
     //创建目标目录
     mkdirsSync(path.dirname(dst));
@@ -163,7 +165,7 @@ var copyFile = function (src, dst, overwrite, callback) {
         if(stat.isFile()){
             if(!overwrite){
                 //是个文件且不能覆盖
-                throw 'File ' + dst + ' is exists.';
+                throw new Error('File ' + dst + ' is exists.');
             }else{
                 fs.unlinkSync(dst);
                 // console.log('--删除 ' , dst);
@@ -195,7 +197,7 @@ var writeFileSync = function(filenName, content, overwrite){
     mkdirsSync(path.dirname(filenName));
     if(fs.existsSync(filenName)){
         if(!overwrite){
-            throw 'File ' + filenName + ' is exists.';
+            throw new Error('File ' + filenName + ' is exists.');
         }else{
             fs.unlinkSync(filenName);
         }
